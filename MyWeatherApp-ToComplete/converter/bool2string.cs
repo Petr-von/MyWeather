f040a212bad68f809a9a9b5d287b2f576e668388
Freezingsmile <409871419@qq.com>
2018-07-04 16:48:19 +0800

    Add files via upload

---
+++ MyWeatherApp-ToComplete/converter/bool2string.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MyWeatherApp.converter
+{
+    class bool2string : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool)value == true)
+            {
+                return "查找路况";
+            } else
+            {
+                return "查找地图";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if ((string)value == "查找地图")
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
